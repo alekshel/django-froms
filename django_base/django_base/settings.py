43f4acfb1393c492a5a3df554f12dd2989329787
alekshel <alek_shel@icloud.com>
2023-10-08 01:27:24 +0600

    Init groups app

--- django_base/django_base/settings.py
+++ django_base/django_base/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "teachers.apps.TeachersConfig",
+    "groups.apps.GroupsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
