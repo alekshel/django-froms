57d1715302d55091a506ae3b832f9550655c4959
alekshel <alek_shel@icloud.com>
2023-10-08 01:41:08 +0600

    Groups task

--- django_base/groups/models.py
+++ django_base/groups/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from teachers.models import Teacher
+
+
+class Group(models.Model):
+    name = models.CharField("Назва групи", max_length=255, default="")
+    teacher = models.ForeignKey(
+        Teacher, verbose_name="Вчитель", on_delete=models.PROTECT
+    )
+
+    def __str__(self):
+        return self.name
